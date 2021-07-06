e11b624b34d0b64891432906008e462b68616130
ZAHARADDINSURAJOHASSAN <86839407+ZAHARADDINSURAJOHASSAN@users.noreply.github.com>
2021-07-06 15:39:32 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -12,3 +12,24 @@
         <div id="header">
             <h1 style= "color:white; font-size:25px; font-family: monospace; ">GAMMA MINI PROJECT GROUP</h1>
         </div>
+		
+		
+		
+		
+		
+        <div id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="add.php">Add</a>
+                </li>
+                <li>
+                    <a href="update.php">Update</a>
+                </li>
+                <li>
+                    <a href="delete.php">Delete</a>
+                </li>
+            </ul>
+        </div>
